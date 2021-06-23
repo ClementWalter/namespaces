ab411e4fda6dd9c485b182f6f573a0d01aa4ed97
Clement Walter <clement.walter@ibm.com>
2021-06-23 16:16:55 +0200

    Print sys.modules

--- top_level_package/subpackage_3/__init__.py
+++ top_level_package/subpackage_3/__init__.py
@@ -1,6 +1,10 @@
 print(__file__)
 globals()["prev"] = "prev"
+import sys
+
+print(sys.modules)
 import top_level_package.subpackage_3.foo
+print(sys.modules)
 
 globals()["next"] = "next"
 print(f"end of {__file__}")
